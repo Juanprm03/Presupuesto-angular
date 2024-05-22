adf044074bd6a4f953b4fa5dea336c10d7c90aaa
unknown <Administrador@MEDAPRCPDFSP109.FormacionSena.RED>
2024-05-22 16:30:52 -0500

    Primer comentario

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,31 @@
+import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 
+// Modulos
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+
+// Componentes
 import { AppComponent } from './app.component';
+import { GastosComponent } from './components/gastos/gastos.component';
+import { IngresarPresupuestoComponent } from './components/ingresar-presupuesto/ingresar-presupuesto.component';
+import { IngresarGastoComponent } from './components/gastos/ingresar-gasto/ingresar-gasto.component';
+import { ListarGastoComponent } from './components/gastos/listar-gasto/listar-gasto.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GastosComponent,
+    IngresarPresupuestoComponent,
+    IngresarGastoComponent,
+    ListarGastoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
-  providers: [
-    provideClientHydration()
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
